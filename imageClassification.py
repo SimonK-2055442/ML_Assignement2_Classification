753880d39c1b96934886702802209e76cb05b95e
SimonK-2055442 <simon_knuts@student.uhasselt.be>
2023-11-21 16:47:59 +0100

    zogoed als af

---
+++ imageClassification.py
@@ -0,0 +1,139 @@
+import os
+
+import random
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import seaborn as sns
+import tensorflow as tf
+from tensorflow import keras
+from keras import layers,models
+from keras.preprocessing import image
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Conv2D, Flatten, MaxPooling2D, Dense, Dropout, GlobalAveragePooling2D
+from keras import optimizers, losses
+
+# System libraries
+from pathlib import Path
+import os.path
+
+# Metrics
+import itertools
+
+
+
+train_data = '../ClassificationV2/dataFruit/train'
+val_data = '../ClassificationV2/dataFruit/test'
+test_data = '../ClassificationV2/dataFruit/predict'
+
+
+def display_random_images(train_data_path):
+    # Get the list of subdirectories (each representing a class)
+    classes = os.listdir(train_data_path)
+
+    # Create a grid of images
+    plt.figure(figsize=(20, 20))
+
+    # Loop through each class folder and display a random image
+    for i, class_name in enumerate(classes):
+        class_path = os.path.join(train_data_path, class_name)
+        class_images = os.listdir(class_path)
+
+        # Select a random image from the class
+        random_image = random.choice(class_images)
+
+        # Load and display the image using plt.imshow
+        image_path = os.path.join(class_path, random_image)
+        img = mpimg.imread(image_path)
+
+        plt.subplot(2, 5, i + 1)
+        plt.imshow(img)
+        plt.title(f"fruit: {class_name}")
+        plt.axis("off")
+
+    plt.show()
+
+
+# Display a random image from each class
+display_random_images(train_data)
+
+#making image augmentators to increase the number of images in the dataset
+train_dataGenerator = ImageDataGenerator(rescale =1. / 255, rotation_range = 40, width_shift_range = 0.2, height_shift_range = 0.2,
+                                         shear_range = 0.2, zoom_range = 0.2, horizontal_flip = True, fill_mode = 'nearest')
+validation_dataGenerator = ImageDataGenerator(rescale =1. / 255, rotation_range = 40, width_shift_range = 0.2, height_shift_range = 0.2,
+                                              shear_range = 0.2, zoom_range = 0.2, horizontal_flip = True, fill_mode = 'nearest')
+test_dataGenerator = ImageDataGenerator(rescale =1. / 255)
+
+#using the image augmentators to create new images
+train_images = train_dataGenerator.flow_from_directory(directory = train_data, batch_size = 32, target_size = (224, 224), class_mode ="categorical", shuffle = False)
+validation_images = validation_dataGenerator.flow_from_directory(directory = val_data, batch_size = 32, target_size = (224, 224), class_mode ="categorical")
+test_images = test_dataGenerator.flow_from_directory(directory = test_data, batch_size = 32, target_size = (224, 224), class_mode ="categorical")
+
+
+
+# Load pretrained Model
+mobile_model = Sequential()
+
+pretrained_model= tf.keras.applications.mobilenet.MobileNet(include_top=False,
+                   input_shape=(224,224,3),
+                   pooling='avg',classes=10,
+                   weights='imagenet')
+for layer in pretrained_model.layers:
+        layer.trainable=False
+
+mobile_model.add(pretrained_model)
+
+
+
+mobile_model.add(Flatten())
+mobile_model.add(Dense(512, activation='relu'))
+mobile_model.add(Dense(128, activation='relu'))
+mobile_model.add(Dropout(0.2))
+mobile_model.add(Dense(10, activation='softmax'))
+
+
+
+mobile_model.summary()
+
+
+
+mobile_model.compile(loss = 'categorical_crossentropy', optimizer = tf.keras.optimizers.Adam(), metrics = ['accuracy'])
+history = mobile_model.fit(train_images, epochs = 20, steps_per_epoch = len(train_images),
+                    validation_data = validation_images, validation_steps = None)
+
+
+
+def plot_loss_curves(history):
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    accuracy = history.history['accuracy']
+    val_accuracy = history.history['val_accuracy']
+
+    epochs = range(len(history.history['loss']))
+
+    plt.plot(epochs, loss, label='training_loss')
+    plt.plot(epochs, val_loss, label='val_loss')
+    plt.title('loss')
+    plt.xlabel('epochs')
+    plt.legend()
+
+    plt.figure()
+    plt.plot(epochs, accuracy, label='training_accuracy')
+    plt.plot(epochs, val_accuracy, label='val_accuracy')
+    plt.title('accuracy')
+    plt.xlabel('epochs')
+    plt.legend()
+
+
+
+plot_loss_curves(history)
+mobile_model.evaluate(validation_images)
+
+
+
+
+
+
---
+++ textClassification.py
@@ -0,0 +1,72 @@
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+
+
+data = pd.read_csv("dataTitanic/titanic.csv")
+print(data.info())
+
+# rijen met nulvalues verwijderen of gokken -> eerste poging met verwijderen
+
+data.dropna(axis=0, subset=["age", "embarked", "fare"], inplace=True)
+data.replace({'sex': {'male': 0, 'female': 1}}, inplace=True)
+
+
+# nuttige features zijn survived, Sex, Age, Fare, SibSp (hoeveelheid echtgenoten broers en zussen aan boord), Parch (hoeveelheid ouders/kinderen aan boord), Pclass (indicatie van sociale klasse -> 1 is hoogste, 3 laagste)
+# alle de rest effe droppen
+
+data.drop(columns=["name", "ticket", "cabin", "embarked", "boat", "body", "home.dest"], inplace=True)
+
+
+# corr matrix maken
+corr_matrix = data[["survived", "sex", "pclass", "parch", "fare"]].corr()
+plt.figure(figsize=(10, 7))
+sns.heatmap(corr_matrix, annot=True, linewidths=0.5, fmt=".2f", cmap="YlGnBu")
+plt.show()
+
+# dataTitanic visualisatie
+fare_bins = [0, 20, 50, 100, 200, 1000]
+fare_labels = ['0-20', '20-50', '50-100', '100-200', '200+']
+fareGroup = pd.cut(data['fare'], bins=fare_bins, labels=fare_labels, right=False)
+
+fig, axes = plt.subplots(2, 2, figsize=(12, 8))
+
+sns.barplot(x="sex", y="survived", data=data, ax=axes[0, 0])
+axes[0, 0].set_title("Survival rate by sex")
+sns.barplot(x="pclass", y="survived", data=data, ax=axes[0, 1])
+axes[0, 1].set_title("Survival rate by pclass")
+sns.barplot(x="parch", y="survived", data=data, ax=axes[1, 0])
+axes[1, 0].set_title("Survival rate by parch")
+sns.barplot(x=fareGroup, y="survived", data=data, ax=axes[1, 1])
+axes[1, 1].set_title("Survival rate by fare")
+
+plt.tight_layout()
+plt.show()
+
+
+features = ["sex", "age", "fare", "sibsp", "parch", "pclass"]
+target = "survived"
+
+X_train, X_test, y_train, y_test = train_test_split(data[features], data[target], test_size=0.2, random_state=42)
+
+
+svm_classifier = SVC(kernel='linear', C=1)
+svm_classifier.fit(X_train, y_train)
+svm_predictions = svm_classifier.predict(X_test)
+
+dt_classifier = DecisionTreeClassifier(random_state=42)
+dt_classifier.fit(X_train, y_train)
+dt_predictions = dt_classifier.predict(X_test)
+
+print("Support Vector Machine Classifier:")
+print("Accuracy:", accuracy_score(y_test, svm_predictions))
+print("Confusion Matrix:\n", confusion_matrix(y_test, svm_predictions))
+
+print("\nDecision Tree Classifier:")
+print("Accuracy:", accuracy_score(y_test, dt_predictions))
+print("Confusion Matrix:\n", confusion_matrix(y_test, dt_predictions))
+
