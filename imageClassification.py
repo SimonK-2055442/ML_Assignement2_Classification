6a39388fc22fe38b48e2c1f147f866b8e3e10d68
SimonK-2055442 <simon_knuts@student.uhasselt.be>
2023-11-28 14:55:14 +0100

    kleine aanpassingen

--- imageClassification.py
+++ imageClassification.py
@@ -21,94 +21,25 @@ import os.path
 # Metrics
 import itertools
 
-train_data = '../ClassificationV2/dataFruit/train'
-val_data = '../ClassificationV2/dataFruit/test'
-test_data = '../ClassificationV2/dataFruit/predict'
 
-def display_random_images(train_data_path):
-    # Get the list of subdirectories (each representing a class)
-    classes = os.listdir(train_data_path)
+# Function to check if a model file exists
+def is_model_saved(filename):
+    return os.path.isfile(filename)
 
-    # Create a grid of images
-    plt.figure(figsize=(20, 20))
-
-    # Loop through each class folder and display a random image
-    for i, class_name in enumerate(classes):
-        class_path = os.path.join(train_data_path, class_name)
-        class_images = os.listdir(class_path)
-
-        # Select a random image from the class
-        random_image = random.choice(class_images)
-
-        # Load and display the image using plt.imshow
-        image_path = os.path.join(class_path, random_image)
-        img = mpimg.imread(image_path)
-
-        plt.subplot(2, 5, i + 1)
-        plt.imshow(img)
-        plt.title(f"fruit: {class_name}")
-        plt.axis("off")
-
-    plt.show()
-
-# Display a random image from each fruit
-display_random_images(train_data)
-
-# Print the amount of pictures from each fruit
-for class_name in os.listdir(train_data):
-    class_path = os.path.join(train_data, class_name)
-    num_images = len(os.listdir(class_path))
-    print(f"{class_name}: {num_images} images")
-
-
-# Making image augmentators to increase the number of images in the dataset
-train_dataGenerator = ImageDataGenerator(rescale=1. / 255, rotation_range=40, width_shift_range=0.2, height_shift_range=0.2,
-                                         shear_range=0.2, zoom_range=0.2, horizontal_flip=True, fill_mode='nearest')
-validation_dataGenerator = ImageDataGenerator(rescale=1. / 255, rotation_range=40, width_shift_range=0.2, height_shift_range=0.2,
-                                              shear_range=0.2, zoom_range=0.2, horizontal_flip=True, fill_mode='nearest')
-test_dataGenerator = ImageDataGenerator(rescale=1. / 255)
-
-# Using the image augmentators to create new images
-train_images = train_dataGenerator.flow_from_directory(directory=train_data, batch_size=32, target_size=(224, 224),
-                                                       class_mode="categorical", shuffle=False)
-validation_images = validation_dataGenerator.flow_from_directory(directory=val_data, batch_size=32, target_size=(224, 224),
-                                                                 class_mode="categorical")
-test_images = test_dataGenerator.flow_from_directory(directory=test_data, batch_size=32, target_size=(224, 224),
-                                                     class_mode="categorical")
-
-# Load pretrained Model
-mobile_model = Sequential()
-
-pretrained_model = tf.keras.applications.mobilenet.MobileNet(include_top=False,
-                                                             input_shape=(224, 224, 3),
-                                                             pooling='avg', classes=10,
-                                                             weights='imagenet')
-for layer in pretrained_model.layers:
-    layer.trainable = False
-
-mobile_model.add(pretrained_model)
-
-mobile_model.add(Flatten())
-mobile_model.add(Dense(512, activation='relu'))
-mobile_model.add(Dense(128, activation='relu'))
-mobile_model.add(Dropout(0.2))
-mobile_model.add(Dense(10, activation='softmax'))
-
-mobile_model.summary()
-
-mobile_model.compile(loss='categorical_crossentropy', optimizer=tf.keras.optimizers.Adam(), metrics=['accuracy'])
-history = mobile_model.fit(train_images, epochs=20, steps_per_epoch=len(train_images),
-                           validation_data=validation_images, validation_steps=None)
+# Function to load the saved model
+def load_model(filename):
+    model = keras.models.load_model(filename)
+    print(f"Model loaded from {filename}")
+    return model
 
 # Function to save the trained model
 def save_model(model, filename):
     model.save(filename)
     print(f"Model saved as {filename}")
 
-save_model(mobile_model, "fruit_classification_model.h5")
-
 # Data visualizations
 def plot_loss_curves(history):
+    plt.figure(figsize=(20, 20))
     loss = history.history['loss']
     val_loss = history.history['val_loss']
 
@@ -129,6 +60,96 @@ def plot_loss_curves(history):
     plt.title('accuracy')
     plt.xlabel('epochs')
     plt.legend()
+    plt.savefig('loss.png')
+    plt.show()
 
-plot_loss_curves(history)
-mobile_model.evaluate(validation_images)
+def display_random_images(train_data_path):
+    classes = os.listdir(train_data_path)
+
+    plt.figure(figsize=(20, 20))
+
+    for i, class_name in enumerate(classes):
+        class_path = os.path.join(train_data_path, class_name)
+        class_images = os.listdir(class_path)
+
+        random_image = random.choice(class_images)
+
+        image_path = os.path.join(class_path, random_image)
+        img = mpimg.imread(image_path)
+
+        plt.subplot(2, 5, i + 1)
+        plt.imshow(img)
+        plt.title(f"fruit: {class_name}")
+        plt.axis("off")
+
+    plt.show()
+
+
+def main():
+
+    train_data = '../ClassificationV2/dataFruit/train'
+    val_data = '../ClassificationV2/dataFruit/test'
+    test_data = '../ClassificationV2/dataFruit/predict'
+
+
+    # Display a random image from each fruit
+    display_random_images(train_data)
+
+    # Print the amount of pictures from each fruit
+    for class_name in os.listdir(train_data):
+        class_path = os.path.join(train_data, class_name)
+        num_images = len(os.listdir(class_path))
+        print(f"{class_name}: {num_images} images")
+
+    # Making image augmentators to increase the number of images in the dataset
+    train_dataGenerator = ImageDataGenerator(rescale=1. / 255, rotation_range=30, width_shift_range=0.2, height_shift_range=0.2,
+                                             shear_range=0.2, zoom_range=0.2, horizontal_flip=True, fill_mode='nearest')
+    validation_dataGenerator = ImageDataGenerator(rescale=1. / 255, rotation_range=30, width_shift_range=0.2, height_shift_range=0.2,
+                                                  shear_range=0.2, zoom_range=0.2, horizontal_flip=True, fill_mode='nearest')
+
+    # Using the image augmentators to create new images
+    train_images = train_dataGenerator.flow_from_directory(directory=train_data, batch_size=32, target_size=(224, 224),
+                                                           class_mode="categorical", shuffle=False)
+    validation_images = validation_dataGenerator.flow_from_directory(directory=val_data, batch_size=32, target_size=(224, 224),
+                                                                     class_mode="categorical")
+
+
+    model_filename = "fruit_classification_model.h5"
+    if is_model_saved(model_filename):
+        # Load the saved model
+        mobile_model = load_model(model_filename)
+
+    else:
+        # Load pretrained Model
+        mobile_model = Sequential()
+
+        pretrained_model = tf.keras.applications.mobilenet.MobileNet(include_top=False,
+                                                                     input_shape=(224, 224, 3),
+                                                                     pooling='avg', classes=10,
+                                                                     weights='imagenet')
+        for layer in pretrained_model.layers:
+            layer.trainable = False
+
+        mobile_model.add(pretrained_model)
+        mobile_model.add(Flatten())
+        mobile_model.add(Dense(512, activation='relu'))
+        mobile_model.add(Dense(128, activation='relu'))
+        mobile_model.add(Dropout(0.2))
+        mobile_model.add(Dense(10, activation='softmax'))
+
+        mobile_model.summary()
+
+        mobile_model.compile(loss='categorical_crossentropy', optimizer=tf.keras.optimizers.Adam(), metrics=['accuracy'])
+        history = mobile_model.fit(train_images, epochs=15, steps_per_epoch=len(train_images),
+                                   validation_data=validation_images, validation_steps=None)
+
+        save_model(mobile_model, "fruit_classification_model.h5")
+
+        plot_loss_curves(history)
+
+
+    mobile_model.evaluate(validation_images)
+
+
+if __name__ == "__main__":
+    main()
--- textClassification.py
+++ textClassification.py
@@ -10,19 +10,29 @@ from sklearn.metrics import accuracy_score, classification_report, confusion_mat
 data = pd.read_csv("dataTitanic/titanic.csv")
 print(data.info())
 
+# useful features are survived, sex, age, fare, sibsp (number of spouses, siblings on board), parch (number of parents, children on board), pclass (indication of social class -> 1 is highest, 3 lowest)
+# drop the rest of the features for now
+data.drop(columns=["name", "ticket", "cabin", "boat", "body", "home.dest"], inplace=True)
+
+print(data.info())
+
+# Assuming 'data' is your DataFrame
+mean_age = data['age'].mean()
+
+# Replace missing values in 'age' with the mean
+data['age'].fillna(mean_age, inplace=True)
 
 # delete or replace null values in row -> first try: delete
 data.dropna(axis=0, subset=["age", "embarked", "fare"], inplace=True)
 data.replace({'sex': {'male': 0, 'female': 1}}, inplace=True)
+data.replace({'embarked': {'S': 0, 'C': 1, 'Q': 2}}, inplace=True)
 
+print(data.info())
 
-# useful features are survived, sex, age, fare, sibsp (number of spouses, siblings on board), parch (number of parents, children on board), pclass (indication of social class -> 1 is highest, 3 lowest)
-# drop the rest of the features for now
-data.drop(columns=["name", "ticket", "cabin", "embarked", "boat", "body", "home.dest"], inplace=True)
 
 
 # making a correlation matrix to see which features are most important
-corr_matrix = data[["survived", "sex", "pclass", "parch", "fare"]].corr()
+corr_matrix = data[["survived", "sex", "pclass", "parch", "fare", "embarked"]].corr()
 plt.figure(figsize=(10, 7))
 sns.heatmap(corr_matrix, annot=True, linewidths=0.5, fmt=".2f", cmap="YlGnBu")
 plt.show()
@@ -31,8 +41,11 @@ plt.show()
 fare_bins = [0, 20, 50, 100, 200, 1000]
 fare_labels = ['0-20', '20-50', '50-100', '100-200', '200+']
 fareGroup = pd.cut(data['fare'], bins=fare_bins, labels=fare_labels, right=False)
+age_bins = [0, 10, 20, 30, 40, 50, 60, 70, 100]
+age_labels = ['0-10', '10-20', '20-30', '30-40', '40-50', '50-60', "60-70", "70+"]
+ageGroup = pd.cut(data['age'], bins=age_bins, labels=age_labels, right=False)
 
-fig, axes = plt.subplots(2, 2, figsize=(12, 8))
+fig, axes = plt.subplots(4, 2, figsize=(12, 8))
 
 sns.barplot(x="sex", y="survived", data=data, ax=axes[0, 0])
 axes[0, 0].set_title("Survival rate by sex")
@@ -42,12 +55,19 @@ sns.barplot(x="parch", y="survived", data=data, ax=axes[1, 0])
 axes[1, 0].set_title("Survival rate by parch")
 sns.barplot(x=fareGroup, y="survived", data=data, ax=axes[1, 1])
 axes[1, 1].set_title("Survival rate by fare")
+sns.barplot(x="embarked", y="survived", data=data, ax=axes[2, 0])
+axes[2, 0].set_title("Survival rate by embarked")
+sns.barplot(x="sibsp", y="survived", data=data, ax=axes[2, 1])
+axes[2, 1].set_title("Survival rate by sibsp")
+sns.barplot(x=ageGroup, y="survived", data=data, ax=axes[3, 0])
+axes[3, 0].set_title("Survival rate by age")
+fig.delaxes(axes[3,1])
 
 plt.tight_layout()
 plt.show()
 
 
-features = ["sex", "age", "fare", "sibsp", "parch", "pclass"]
+features = ["sex", "age", "fare", "sibsp", "parch", "pclass", "embarked"]
 target = "survived"
 
 X_train, X_test, y_train, y_test = train_test_split(data[features], data[target], test_size=0.2, random_state=42)
